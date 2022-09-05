3be80f94b3202ccd4f4bf863b10f7b55bb000233
talithaolga <112867377+talithaolga@users.noreply.github.com>
2022-09-05 21:20:30 +0700

    Add files via upload

---
+++ Latihan 1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Halo kenalin namaku Talitha Olga Ramadhani" << endl;
+    cout << "NIM A11.2022.14110" << endl;
+    cout << "Alamat rumahku ada di Jl. Parang Klitik 3" << endl;
+    cout << "Aku berasal dari SMAN 10 Semarang" << endl;
+    return 0;
+}
