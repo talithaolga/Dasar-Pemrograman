962e8828263d9e10fdca2bd8cfdd4025317a8bbb
talithaolga <112867377+talithaolga@users.noreply.github.com>
2022-09-12 21:15:42 +0700

    Add files via upload

---
+++ Pertemuan 3/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Halo gais, kenalin namaku Talitha Olga Ramadhani" << endl;
+    cout << "NIM : A11.2022.14110" << endl;
+    cout << "-----------------------------------" << endl;
+    cout << "Dibawah ini adalah deksripsi diriku" << endl;
+    cout << "-----------------------------------" << endl;
+    /*
+    printf ("Test");
+    int=integer
+    char=character
+    */
+    int u = 17;
+    float t = 150.0;
+    char b = 'M';
+    string ub = "XS";
+
+    cout << "Umur: " << u;
+    cout << "\nTinggi badan: " << t;
+    cout << "\nUkuran baju: " << b;
+    cout << "\nUkuran baju: " << ub;
+    return 0;
+}
